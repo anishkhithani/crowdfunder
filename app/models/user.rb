07448bd8f171abbb6718abe05e8f674e77341fac
Anish Khithani <anish.khithani@gmail.com>
2013-07-19 14:31:27 -0400

    Generating Pledge model and adding associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :first_name, :last_name, :email, :password
+  has_many :pledges
   has_many :projects
   validates_presence_of :email
   validates_presence_of :password
