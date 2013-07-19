af71985baf2a23d615cd7f81b5d026fdd6cfd19b
Anish Khithani <anish.khithani@gmail.com>
2013-07-19 13:52:23 -0400

    writing tests for errors on user registration form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       redirect_to root_path, :notice => "Account created"
 
     else
+      flash[:alert] = "Try again"
       render :new
     end
   end
